013689dfeb3111a66a2f25760c040f7bb5c55bc4
fedesilveroutn <fedesilveromusico@gmail.com>
2021-11-26 09:04:51 -0300

    Creacion del proyecto

---
+++ SP_Labo1_SilveroFedericoElias/src/SP_Labo1_SilveroFedericoElias.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : SP_Labo1_SilveroFedericoElias.c
+ Author      : Federico Elías Silvero
+ Version     : Segundo parcial
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setbuf(stdout, NULL);
+
+
+	return 0;
+}
